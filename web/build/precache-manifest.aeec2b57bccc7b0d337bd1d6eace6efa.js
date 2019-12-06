34bcee24711a0aa8ad538dbbc7db28928b04f243
Thijs W <mawijnen2@student.avans.nl>
2019-12-06 01:33:45 +0100

    added web components

---
+++ web/build/precache-manifest.aeec2b57bccc7b0d337bd1d6eace6efa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b66242c8c93a89021ff1b28cd9f944",
+    "url": "/puremonk-web/index.html"
+  },
+  {
+    "revision": "bb566e3bd9c1e8e0dcaa",
+    "url": "/puremonk-web/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1a672d976ebb4c486008",
+    "url": "/puremonk-web/static/js/2.6fb4ea39.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/puremonk-web/static/js/2.6fb4ea39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb566e3bd9c1e8e0dcaa",
+    "url": "/puremonk-web/static/js/main.fe40452d.chunk.js"
+  },
+  {
+    "revision": "3de164b5a0a621e7a3b3",
+    "url": "/puremonk-web/static/js/runtime-main.e7b12f46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/puremonk-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
