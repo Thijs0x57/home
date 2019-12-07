ef56ca1659becd9025cb0f9304a571fba01f7f92
Thijs W <mawijnen2@student.avans.nl>
2019-12-07 11:22:22 +0100

    Updates

---
+++ precache-manifest.98ef17e66903bf5cd55f17382b394fad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d259afaddd92bf95563ae9ccf13cc08",
+    "url": "/puremonk-web/index.html"
+  },
+  {
+    "revision": "497b52ddf47841d7abb9",
+    "url": "/puremonk-web/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "1a672d976ebb4c486008",
+    "url": "/puremonk-web/static/js/2.6fb4ea39.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/puremonk-web/static/js/2.6fb4ea39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "497b52ddf47841d7abb9",
+    "url": "/puremonk-web/static/js/main.aa094b57.chunk.js"
+  },
+  {
+    "revision": "3de164b5a0a621e7a3b3",
+    "url": "/puremonk-web/static/js/runtime-main.e7b12f46.js"
+  },
+  {
+    "revision": "bebf02213a9c2c2478ac3ffb39d595c8",
+    "url": "/puremonk-web/static/media/face.bebf0221.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fed0d1732eaee52202ed57d5b8246df3.js"
+  "/puremonk-web/precache-manifest.98ef17e66903bf5cd55f17382b394fad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/puremonk-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
