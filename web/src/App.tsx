d2f1da475a854a1355aa551e1459d534154fca6d
Thijs W <mawijnen2@student.avans.nl>
2019-12-09 23:37:07 +0100

    changed homepage

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import logo from './assets/face.png';
+import monitor from './assets/programming-monitor.png';
 import './App.css';
 
 
 console.log("test2: ", process.env.PUBLIC_URL);
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Puremonk.net
-          <div>A work in progress</div>
-        </p>
-      </header>
+    <div className="App box">
+        <div className="logo box">
+          <img src={monitor} className="App-logo box" alt="logo" />
+          <p className="App-logo-text box">Puremonk</p>
+        </div>
+        <p className="wip box">A work in progress...</p>
     </div>
   );
 }
